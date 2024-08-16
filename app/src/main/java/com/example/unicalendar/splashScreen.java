af8d290cd61b7d0943e48c603b1918116f045947
Venu Virparia <venupatel004@gmail.com>
2024-08-16 16:24:40 +0530

    Add Splash Screen

---
+++ app/src/main/java/com/example/unicalendar/splashScreen.java
@@ -0,0 +1,29 @@
+package com.example.unicalendar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splashScreen.this, MainActivity.class));
+                finish();  // Optional: Close the splash screen activity after starting MainActivity
+            }
+        }, 3000);
+    }
+}
